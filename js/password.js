3c0497433e103293857817e186331b790c81fad9
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-21 15:07:16 -0500

    changed help text display method

--- js/password.js
+++ js/password.js
@@ -3,7 +3,15 @@
 function checkPassword() {
     var pass = document.getElementById("pass").value;
     var passValidation = document.getElementById("passValidate").value;
-    if (pass === passValidation && pass.length() >= 8) {
-        
+    var helpText = document.getElementsByClassName("p.help")[0];
+    helpText.value = "";
+    if (pass === passValidation) {
+        document.getElementById("helpMatch").innerText += "Entered passwords do not match! Try again.\n";
+    }
+    else if (pass.length() >= 8) {
+        document.getElementById("helpMessage").innerText = "Entered password is too short! Enter a password longer than 8 characters.\n";
+    }
+    else {
+        document.write("<p>Valid password</p>.");
     }
 }
