e516b7a44ac6227804f3e722be148e9e194dd2f3
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-21 15:22:01 -0500

    fixed errors in js

--- js/password.js
+++ js/password.js
@@ -3,15 +3,18 @@
 function checkPassword() {
     var pass = document.getElementById("pass").value;
     var passValidation = document.getElementById("passValidate").value;
-    var helpText = document.getElementsByClassName("p.help")[0];
-    helpText.value = "";
-    if (pass === passValidation) {
-        document.getElementById("helpMatch").innerText += "Entered passwords do not match! Try again.\n";
+    var helpText = document.getElementById("help");
+    helpText.innerText = "";
+    if (pass !== passValidation) {
+        helpText.innerText = "Entered passwords do not match! Try again.";
+        helpText.style.color = "red";
     }
-    else if (pass.length() >= 8) {
-        document.getElementById("helpMessage").innerText = "Entered password is too short! Enter a password longer than 8 characters.\n";
+    else if (pass.length < 8) {
+        helpText.innerText = "Entered password is too short! Enter a password longer than 8 characters.";
+        helpText.style.color = "red";
     }
     else {
-        document.write("<p>Valid password</p>.");
+        helpText.innerText = "Valid Password!";
+        helpText.style.color = "green";
     }
 }
