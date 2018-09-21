051de971e3cd8edb638ac43369ec8882d4325cbb
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-21 14:34:43 -0500

    Wrote check password function

---
+++ js/password.js
@@ -0,0 +1,9 @@
+
+
+function checkPassword() {
+    var pass = document.getElementById("pass").value;
+    var passValidation = document.getElementById("passValidate").value;
+    if (pass === passValidation && pass.length() >= 8) {
+        
+    }
+}
