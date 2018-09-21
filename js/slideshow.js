4eb3fd6af8582f1e8b74ae17ea757bd3cb7495ca
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-21 15:46:32 -0500

    created slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,23 @@
+var currentImg = 1;
+var nImages = 5;
+
+function setImgString() {
+    document.getElementById("img").src= `img/pic${currentImg}.jpeg`;
+}
+
+function nextSlide() {
+    if (currentImg === nImages) {
+        currentImg = 1;
+    }
+    else currentImg++;
+    setImgString();
+}
+
+function prevSlide() {
+    if (currentImg === 1) {
+        currentImg = nImages;
+    }
+    else currentImg--;
+    setImgString();
+}
+
